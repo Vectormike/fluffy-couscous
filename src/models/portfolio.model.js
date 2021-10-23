f72e31afd2ca2e8683f21f03920aab2189c02229
Victor Jonah <victorjonah199@gmail.com>
2021-10-23 13:38:26 +0100

    chore: project init

---
+++ src/models/portfolio.model.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose');
+const { toJSON, paginate } = require('./plugins');
+
+const portfolioSchema = mongoose.Schema(
+  {
+    user: {
+      type: String,
+      ref: 'User',
+      required: true,
+    },
+    symbol: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    totalQuantity: {
+      type: Number,
+      required: true,
+    },
+    equityValue: { type: Number, required: true },
+    pricePerShare: { type: Number, required: true },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// add plugin that converts mongoose to json
+portfolioSchema.plugin(toJSON);
+portfolioSchema.plugin(paginate);
+
+/**
+ * Check if symbol is taken
+ * @returns {Promise<boolean>}
+ */
+portfolioSchema.statics.isEmailTaken = async function (symbol, excludeUserId) {
+  const porfolio = await this.findOne({ symbol, _id: { $ne: excludeUserId } });
+  return !!porfolio;
+};
+
+/**
+ * @typedef Portfolio
+ */
+const Portfolio = mongoose.model('Portfolio', portfolioSchema);
+
+module.exports = Portfolio;
